1185aa8f5e75ff143639de380bf95560ae106375
wlswotmd <umsun2357@korea.ac.kr>
2024-06-08 20:33:22 +0900

    컴파일 에러 수정

--- include/uallsyms/uallsyms.h
+++ include/uallsyms/uallsyms.h
@@ -1,7 +1,7 @@
 #ifndef _LIBUALLSYMS_H
 #define _LIBUALLSYMS_H
 
-#include <libuallsyms/types.h>
+#include <uallsyms/types.h>
 
 extern kaddr_t uas_lookup_name(uas_t *uas, const char *name);
 extern uas_t *uas_init(uas_aar_t aar_func);
